7690fc4e4a67c8b2ace6d80e4202217841f53ec0
Arturo Deloiza <deloizarodriguezarturo@gmail.com>
2026-06-15 20:54:31 -0600

    playground: Actualizado a Sección 9

--- playground/minilibrary/models.py
+++ playground/minilibrary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Author(models.Model):
@@ -8,6 +9,10 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
@@ -16,5 +21,50 @@ class Book(models.Model):
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50)
 
+    ## Modelado Muchos a Muchos
+    genres = models.ManyToManyField(Genre, related_name = 'books')
+    
+    ## Tabla intermedia personalizada
+    recommended_by = models.ManyToManyField(get_user_model(), through="Recomendation", related_name="recommendations")
+
     def __str__(self):
         return self.title
+    
+
+## Modelado Uno a Uno    
+class BookDetail(models.Model):
+    summary = models.TextField()
+    cover_url = models.CharField()
+    language = models.CharField()
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name = 'detail')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} -> {self.book.title} - ({self.rating}/5)"
+    
+class Loans(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="loans")
+    loan_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank = True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} -> {self.book.title} - ({'Devuelto' if self.is_returned else 'Prestado'})"
+    
+class Recomendation(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    recommended_at = models.DateField(auto_now_add=True)
+    note = models.TextField(blank=True)
+
+    # Esto funciona como un settings, es una clase de configuración:
+    class Meta:
+        unique_together = ("user", "book")
